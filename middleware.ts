2c1b0de7bdebe41a5923f9b37ad93a68ae456b5a
Jackson Huang <jacksonh201325@gmail.com>
2024-04-08 22:14:11 +0200

    added console

--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,7 @@ import { NextRequest, NextResponse } from 'next/server';
 export function middleware(request: NextRequest) {
   const sessionToken = request.cookies.get('next-auth.session-token');
   const { pathname } = request.nextUrl;
-
+  console.log(request.cookies);
   console.log('middleware.ts', sessionToken, pathname);
 
   // if (!sessionToken && pathname === '/') {
