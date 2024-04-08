241c47fde81487e2379f3216fc97771e886d015e
Jackson Huang <jacksonh201325@gmail.com>
2024-04-08 22:20:56 +0200

    small test

--- middleware.ts
+++ middleware.ts
@@ -1,14 +1,18 @@
 import { NextRequest, NextResponse } from 'next/server';
 
 export function middleware(request: NextRequest) {
-  const sessionToken = request.cookies.get('next-auth.session-token');
+  const sessionToken =
+    request.cookies.get('next-auth.session-token') ||
+    request.cookies.get('_vercel_jwt_')?.value;
   const { pathname } = request.nextUrl;
+
+  console.log('request', request);
   console.log('cookies', request.cookies);
   console.log('middleware.ts', sessionToken, pathname);
 
-  // if (!sessionToken && pathname === '/') {
-  //   return NextResponse.redirect(new URL('/gifts', request.url));
-  // }
+  if (!sessionToken && pathname === '/') {
+    return NextResponse.redirect(new URL('/gifts', request.url));
+  }
 
   return NextResponse.next();
 }
