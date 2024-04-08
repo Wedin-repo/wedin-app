09160447c8902010a997315dcf2cae6ff47c733d
Jackson Huang <jacksonh201325@gmail.com>
2024-04-08 22:02:53 +0200

    smallf ix

--- middleware.ts
+++ middleware.ts
@@ -4,9 +4,11 @@ export function middleware(request: NextRequest) {
   const sessionToken = request.cookies.get('next-auth.session-token');
   const { pathname } = request.nextUrl;
 
-  if (!sessionToken && pathname === '/') {
-    return NextResponse.redirect(new URL('/gifts', request.url));
-  }
+  console.log('middleware.ts', sessionToken, pathname);
+
+  // if (!sessionToken && pathname === '/') {
+  //   return NextResponse.redirect(new URL('/gifts', request.url));
+  // }
 
   return NextResponse.next();
 }
