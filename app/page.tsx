df0b69e63eda259d4fceb3ad952c4272f05ba7c1
lordalejandro <alemalladaps5@gmail.com>
2024-04-12 18:31:22 -0400

    fix build error

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ export default async function Home() {
       <div className="min-h-screen flex flex-col justify-center items-center w-full">
         hi, go to /dashboard. <br />
         <p className="flex gap-1">
-          if you can't go because you're disabled just{' '}
+          if you can&apos;t go because you&apos;re disabled just{' '}
           <Link href="/dashboard" className="hover:underline text-blue-500">
             click here
           </Link>
