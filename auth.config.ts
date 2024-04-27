53da098d9f8fbe6eeadb57e9f95e5db56e156714
Jackson Huang <jacksonh201325@gmail.com>
2024-04-28 00:34:49 +0200

    Moved auth config

---
+++ auth.config.ts
@@ -0,0 +1,55 @@
+import prisma from '@/db/client';
+import bcrypt from 'bcryptjs';
+import Credentials from 'next-auth/providers/credentials';
+import Facebook from 'next-auth/providers/facebook';
+import Google from 'next-auth/providers/google';
+import { LoginSchema } from '@/schemas';
+import { NextAuthConfig } from 'next-auth';
+
+const authOptions: NextAuthConfig = {
+  providers: [
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+      allowDangerousEmailAccountLinking: true,
+    }),
+    Facebook({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+      allowDangerousEmailAccountLinking: true,
+    }),
+    Credentials({
+      async authorize(credentials) {
+        const validatedFields = LoginSchema.safeParse(credentials);
+
+        if (validatedFields.success) {
+          const { email, password } = validatedFields.data;
+
+          const user = await prisma.user.findUnique({
+            where: { email },
+          });
+
+          if (!user) {
+            return null;
+          }
+
+          if (!user.password) {
+            return null;
+          }
+
+          const isValid = await bcrypt.compare(password, user.password);
+
+          if (!isValid) {
+            return null;
+          }
+
+          return user;
+        }
+
+        return null;
+      },
+    }),
+  ],
+};
+
+export default authOptions;
--- auth.ts
+++ auth.ts
@@ -1,10 +1,5 @@
-import prisma from '@/db/client';
-import bcrypt from 'bcryptjs';
 import NextAuth from 'next-auth';
-import Credentials from 'next-auth/providers/credentials';
-import Facebook from 'next-auth/providers/facebook';
-import Google from 'next-auth/providers/google';
-import { LoginSchema } from '@/schemas';
+import authOptions from './auth.config';
 
 export const {
   handlers: { GET, POST },
@@ -12,53 +7,11 @@ export const {
   signIn,
   signOut,
 } = NextAuth({
-  providers: [
-    Google({
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-      allowDangerousEmailAccountLinking: true,
-    }),
-    Facebook({
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-      allowDangerousEmailAccountLinking: true,
-    }),
-    Credentials({
-      async authorize(credentials) {
-        const validatedFields = LoginSchema.safeParse(credentials);
-
-        if (validatedFields.success) {
-          const { email, password } = validatedFields.data;
-
-          const user = await prisma.user.findUnique({
-            where: { email },
-          });
-
-          if (!user) {
-            return null;
-          }
-
-          if (!user.password) {
-            return null;
-          }
-
-          const isValid = await bcrypt.compare(password, user.password);
-
-          if (!isValid) {
-            return null;
-          }
-
-          return user;
-        }
-
-        return null;
-      },
-    }),
-  ],
   debug: process.env.NODE_ENV === 'development',
   session: {
     strategy: 'jwt',
     maxAge: 30 * 24 * 60 * 60,
   },
   secret: process.env.NEXTAUTH_SECRET,
+  ...authOptions,
 });
