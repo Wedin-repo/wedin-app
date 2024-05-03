2f105b7ad57977ce71d2ab3946e2f59716a6258b
Jackson Huang <jacksonh201325@gmail.com>
2024-05-03 20:54:23 +0200

    Comment resend for now

--- lib/mail.ts
+++ lib/mail.ts
@@ -1,16 +1,16 @@
 import { Resend } from 'resend';
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+// const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const sendVericationEmail = async (email: string, token: string) => {
   const confirmationLink = `http://localhost:3000/email-verify?token=${token}&email=${email}`;
 
-  await resend.emails.send({
-    from: 'onboarding@resend.dev',
-    to: email,
-    subject: 'Verify your email',
-    html: `<p>Click the link below to verify your <a href="${confirmationLink}">email address</a></p>`,
-  });
+  // await resend.emails.send({
+  //   from: 'onboarding@resend.dev',
+  //   to: email,
+  //   subject: 'Verify your email',
+  //   html: `<p>Click the link below to verify your <a href="${confirmationLink}">email address</a></p>`,
+  // });
 };
 
 export const sendPasswordResetEmail = async (email: string, token: string) => {
